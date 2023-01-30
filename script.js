7870f7db18dd6328b45052d3a3f2a9981877c2be
mtaman <mohamedtaman2012@gmail.com>
2023-01-30 12:55:36 +0300

    --Adding Dropdown menu brought into view on :hover, :focus, and are navigable using either the tab key or by using the keyboard arrow keys. --Adding material Icon

---
+++ script.js
@@ -0,0 +1,9 @@
+let hasDropdown = document.getElementsByClassName("has-dropdown");
+let dropdown = document.getElementsByClassName("dropdown");
+
+//hasDropdown[0].addEventListener('focus', openDropdown);
+// function openDropdown() {
+//     dropdown[0].style.display = 'block';
+//     console.log(dropdown);
+// }
+
