9b7acdcc3522f24a17d362c0f946932855de9d34
mtaman <mohamedtaman2012@gmail.com>
2023-01-30 23:54:51 +0300

    Adding Offcaanves

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("overlay").style.display = "block";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("overlay").style.display = "none";
+
+}
+
+
