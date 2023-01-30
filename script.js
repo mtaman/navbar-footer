dcf039aa46977f982666840e715913324f59ba88
mtaman <mohamedtaman2012@gmail.com>
2023-01-30 13:36:34 +0300

    Adding footer

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-let hasDropdown = document.getElementsByClassName("has-dropdown");
-let dropdown = document.getElementsByClassName("dropdown");
-
-//hasDropdown[0].addEventListener('focus', openDropdown);
-// function openDropdown() {
-//     dropdown[0].style.display = 'block';
-//     console.log(dropdown);
-// }
 
